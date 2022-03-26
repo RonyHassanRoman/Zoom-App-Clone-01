767076db672b656a04ad8941ac2d6498335ee2b0
Rony <ronyhassanroman2@gmail.com>
2022-03-26 20:14:46 +0600

    Zoom app clone

---
+++ lib/screens/auth/login_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:zoom_clone/controllers/auth_controllers.dart';
+
+import '../../widgets/custom_button.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Start to join meetings",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Image.asset('assets/images/onboarding.jpg'),
+            CustomButton(
+              text: 'login',
+              onPressed: () {
+                AuthController().signinWithGoogle();
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const backgroudColor = Color.fromRGBO(36, 36, 36, 1);
+const buttonColor = Color.fromRGBO(14, 114, 236, 1);
+const footerColor = Color.fromRGBO(26, 26, 26, 1);
+const seconderybackgroundColor = Color.fromRGBO(46, 46, 46, 1);
