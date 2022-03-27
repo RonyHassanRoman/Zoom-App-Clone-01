24f65461e67d78469a532735024a555cb9b515e9
Rony <ronyhassanroman2@gmail.com>
2022-03-27 21:58:25 +0600

    firestore

--- lib/controllers/auth_controllers.dart
+++ lib/controllers/auth_controllers.dart
@@ -1,8 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthController {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final _firebaseStore = FirebaseFirestore.instance;
   signinWithGoogle() async {
     try {
       GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
@@ -14,6 +16,17 @@ class AuthController {
       );
       UserCredential userCredential =
           await _auth.signInWithCredential(credential);
+      User? user = userCredential.user;
+      if (user != null) {
+        if (userCredential.additionalUserInfo!.isNewUser) {
+          //we want to save the users data in firebase firestore
+          _firebaseStore.collection('users').doc(user.uid).set({
+            'username': user.displayName,
+            'uid': user.uid,
+            'userImage': user.photoURL,
+          });
+        } else {}
+      }
     } catch (e) {}
   }
 }
